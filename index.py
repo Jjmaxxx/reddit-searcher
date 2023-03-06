1321ceee6108b26e9f21d8faf24282d9afe2e3a7
Justin <justlee888@gmail.com>
2023-03-05 20:13:59 -0800

    broken, fix it

--- index.py
+++ index.py
@@ -14,11 +14,14 @@ options.add_experimental_option("prefs",prefs)
 driver = webdriver.Chrome(service=service,options=options)
 driver.get("https://www.reddit.com/")
 wordsDict = {}
-
+numOfPosts= 0
+num=0
 search = driver.find_element(By.ID,"header-search-bar")
 search.send_keys(playerInput)
-search.send_keys(Keys.RETURN)
-
+WebDriverWait(driver,10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-testid='search-trigger-item']"))
+).click()
+# driver.find_element(By.CSS_SELECTOR,"div[data-testid='search-trigger-item']").click()
 #wait for element to exist before searching if it exists
 try:
     with open('redditPosts.csv','w', encoding="utf-8") as file:
@@ -30,24 +33,74 @@ try:
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR,"div[data-testid='post-container']"))
             )
             for post in posts:
-                upvotes = post.find_element(By.CLASS_NAME,"_vaFo96phV6L5Hltvwcox").text
+                print("next")
+                numOfPosts+=1
+                upvotes = WebDriverWait(post, 20).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, "_vaFo96phV6L5Hltvwcox"))
+                ).text
+                #post.find_element(By.CLASS_NAME,"_vaFo96phV6L5Hltvwcox").text
                 findK =upvotes.find("k")
                 if(findK!= -1):
                     upvotes = int(float(upvotes[0:findK]) * 1000)
                 else:
                     upvotes=upvotes[0:upvotes.find(" ")]
-                postTitle = post.find_element(By.CSS_SELECTOR,"div[data-testid='post-title']")
+                
+                postTitle = WebDriverWait(post, 20).until(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, "div[data-testid='post-title']"))
+                )
+                
+                # post.find_element(By.CSS_SELECTOR,"div[data-testid='post-title']")
                 title = postTitle.find_element(By.TAG_NAME,"h3").text
-                #if _2c1ElNxHftd8W_nZtcG9zf _33Pa96SGhFVpZeI6a7Y_Pl _2r9BZFotuBbLYnijAaskeZ class name exists, then theres a picture/video
+                words = title.split()
+                words.append("posts")
+                if post.find_elements(By.CLASS_NAME,"PrfaR-luawcEBK2dhGuDp"):
+                    words.append("image")
+
                 #check if paragraph class exists when clicked 
+                num+=1
+                if(num == 1):
+                    WebDriverWait(driver, 20).until(
+                        EC.element_to_be_clickable(post)
+                    ).click()
                 # post.click()
-                # postContent = WebDriverWait(driver,10).until(
+                print("post clicked")
+                # postContent = WebDriverWait(post,10).until(
                 #     EC.presence_of_element_located((By.CSS_SELECTOR,"div[data-testid='post-content']"))
                 # )
+                insidePost = WebDriverWait(driver,10).until(
+                    EC.presence_of_element_located((By.ID,"overlayScrollContainer"))
+                )
+                # print(insidePost)
+                # driver.execute_script("overlayScrollContainer.scrollTo(0, overlayScrollContainer.scrollHeight);")
+                paragraphs = WebDriverWait(insidePost,10).until(
+                    EC.presence_of_all_elements_located((By.TAG_NAME,"p"))
+                )
+                #paragraphs = insidePost.find_elements(By.TAG_NAME,"p")
+                if paragraphs:
+                    print("Found")
+                    for p in paragraphs:
+                        words.append(p.text)
+                # postsText = WebDriverWait(post,10).until(
+                #     EC.presence_of_all_elements_located((By.TAG_NAME,"p"))
+                # )
+                # for texts in postsText:
+                #     words.append(texts.text)
+
                 # content = postContent.text
-                # driver.find_element(By.TAG_NAME,"body").send_keys(Keys.ESCAPE)
+                
+                close = driver.find_element(By.XPATH,"//button[@title='Close']")
+                print(close)
+                close.click()
+                #WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'subredditvars-r-nfl')))
+                
+                # driver.find_element(By.XPATH,"//button[@title='Close']").click()
+                # print(header)
+                # WebDriverWait(header, 20).until(
+                #     EC.element_to_be_clickable((By.TAG_NAME,"button"))
+                # ).click()                
+                # driver.find_element(By.ID,"2x-container").send_keys(Keys.ESCAPE)
                 # title = title + " " + content
-                words = title.split()
+                
                 for word in words:
                     word = word.lower()
                     if any(not char.isalnum() for char in word):
@@ -56,7 +109,7 @@ try:
                         wordsDict[word]+=1
                     else:
                         wordsDict[word]=1
-                    #print(word+ ": "+str(wordsDict[word]))
+                    print(word+ ": "+str(wordsDict[word]))
 
                 writer.writerow([title,upvotes])
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -65,6 +118,7 @@ try:
         
         for word in wordsDict:
             writer.writerow([word,wordsDict[word]])
+    print(numOfPosts)
     print("done")
 except Exception as e:
     print(e)
